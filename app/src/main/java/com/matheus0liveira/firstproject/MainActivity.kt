d2f67a5226eca069e1576dcb9bc14133107bc0c0
Matheus0liveiras <matheusfilho98.ms@gmail.com>
2023-12-01 19:47:03 -0300

    feat: create project

---
+++ app/src/main/java/com/matheus0liveira/firstproject/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.matheus0liveira.firstproject
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.matheus0liveira.firstproject.ui.theme.FirstProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val result = 2 / 2
+
+        Log.i("Test", "Hello world")
+
+        setContentView(R.layout.activity_main)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirstProjectTheme {
+        Greeting("Android")
+    }
+}
